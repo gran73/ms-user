755ccb015a5947af60ebca0a5df80f94c5017027
Gurinder Randhawa <gur4ever@hotmail.com>
2020-04-22 12:17:06 -0400

    Fix sql error at startup

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,10 +1,3 @@
 insert into USERS (NAME, EMAIL, PASSWORD) VALUES ('Jyoti', 'jpanchun@gmail.com', 'test1');
 insert into USERS (NAME, EMAIL, PASSWORD) VALUES ('Gurinder', 'gur4ever@hotmail.com', 'test2');
 insert into USERS (NAME, EMAIL, PASSWORD) VALUES ('Dan', 'dan@c.com', 'test3');
-
-#insert into PURCHASES (PRODUCT, PURCHASE_DATE, CUSTOMER_ID)
-#values('Mountain Bike','2010-05-12 00:00:00.0',1);
-#insert into PURCHASES (PRODUCT, PURCHASE_DATE, CUSTOMER_ID)
-#values('Football','2010-04-30 00:00:00.0',2);
-#insert into PURCHASES (PRODUCT, PURCHASE_DATE, CUSTOMER_ID)
-#values('Kayak','2010-06-05 00:00:00.0',3);
